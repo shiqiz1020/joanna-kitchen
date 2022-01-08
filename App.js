816e5843398d80c467ce45e1266dabedc6dcddf2
Shiqi Zhang <shiqiz1020@gmail.com>
2022-01-08 13:36:03 -0800

    added screens and navigations

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import MealsNavigator from './navigation/MealsNavigator';
+import MealDetailScreen from './screens/MealDetailScreen';
+
+const fetchFonts = () => {
+    Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  // return (
+  //     <Text>Open up App.js to start working on your app!</Text>
+  // );
+  return <MealsNavigator />
+}
+
+const styles = StyleSheet.create({
+
+});
